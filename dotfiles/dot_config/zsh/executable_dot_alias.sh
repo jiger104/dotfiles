6aaa122f7114b7f3cf630d2c6a4674627ba66978
Jigar Patel <jigar@automationzen.com>
2025-03-09 17:33:05 -0500

    Refine macOS power management and Python package installation settings

---
+++ dotfiles/dot_config/zsh/executable_dot_alias.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+alias l="ls -lF ${colorflag}"
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias reload="exec ${SHELL} -l"
+alias path='echo -e ${PATH//:/\\n}'
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ~="cd ~" # `cd` is probably faster to type though
+alias -- -="cd -"
+alias zshconfig="mate ~/.zshrc"
+alias ohmyzsh="mate ~/.oh-my-zsh"
---
+++ dotfiles/dot_config/zsh/executable_dot_zshenv.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export PATH="$HOME/.nvm/versions/node/v17.0.1/bin/node:$PATH"
+export PATH="$HOME/.poetry/bin:$PATH"
+export PATH=${0:A:h}/bin:$PATH
+export PATH="$HOMEBREW_PREFIX/opt/python@3.11/libexec/bin:$PATH"
+export POETRY_CONFIG_DIR="$HOME/Library/Preferences/pypoetry"
+export NVM_DIR="$HOME/.nvm"
+export GOOGLE_APPLICATION_CREDENTIALS="$HOME/Coding/Python Projects/That Pitch/that-pitch-service-account.json"
